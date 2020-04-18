bfc06968856f131edfbed5bd6ffc545b9f14739a
Cooper <cooper.pender@gmail.com>
2020-04-17 20:51:16 -0600

    Messed around with go, got some mustache implemented.

--- telephono/telephono_request_template_test.go
+++ telephono/telephono_request_template_test.go
@@ -19,6 +19,7 @@ func TestBasicCallTemplate(t *testing.T) {
 	contributor.Set("host", "https://httpbin.org")
 	contributor.Set("A", "AAAA")
 	contributor.Set("B", "BBBB")
+	contributor.Set("Status", "329")
 	expander.AddContributor(contributor)
 
 	// Execute call
@@ -35,16 +36,22 @@ func TestBasicCallTemplate(t *testing.T) {
 	response, requestErr := templateUnderTest.executeWithClientAndExpander(http.DefaultClient, expander)
 
 	if requestErr != nil {
-		t.Error("Got an error!\n", requestErr.Error())
+		t.Fatal("Got an error!\n", requestErr.Error())
 	}
+
+	t.Run("Check Status Code", func(sub *testing.T) {
+		if response.StatusCode != 200 {
+			t.Fatal("Got an error!", response.StatusCode)
+		}
+	})
 
 	if allBytes, readErr := ioutil.ReadAll(response.Body); readErr == nil {
 		asString := string(allBytes)
 		log.Println(asString)
 		if !(strings.Contains(asString, "BBBB") && strings.Contains(asString, "AAAA")) {
-			t.Error("Didn't find AAAA and BBBB")
+			t.Fatal("Didn't find AAAA and BBBB")
 		}
 	} else {
-		t.Error(readErr.Error())
+		t.Fatal(readErr.Error())
 	}
 }
