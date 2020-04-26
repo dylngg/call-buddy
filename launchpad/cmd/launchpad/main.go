51333ebd961ee88c0b370c8322a0f4d57a70594c
dylngg <dylan@gardnermedia.com>
2020-04-25 22:18:08 -0600

    Create simple pull-based file syncing mechanism in launchpad
    The basic idea here is that we have a goroutine that polls every 3s for
    updates to the remote directory that stores call-buddy data
    (~/.call-buddy) and then syncs things down when they've been modified.
    This is mostly just a proof of concept.

--- launchpad/cmd/launchpad/main.go
+++ launchpad/cmd/launchpad/main.go
@@ -13,6 +13,8 @@ import (
 	"os/user"
 	"path/filepath"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
@@ -25,6 +27,16 @@ type remoteSyncClient struct {
 	sshSession *ssh.Session
 	sftpClient *sftp.Client
 }
+
+type remoteFileState struct {
+	localPath  string
+	remotePath string
+	// Map between files we've seen and the last time they were modified
+	modTimes map[string]time.Time
+}
+
+var quit = make(chan struct{})
+var waitGroup = sync.WaitGroup{}
 
 func parseArgs() (target, arch *string, args []string) {
 	arch = flag.String("a", "", "Architecture to use: e.g. linux-amd64, darwin-amd64, linux-arm")
@@ -162,11 +174,13 @@ func bootstrapCallBuddy(remoteSftpClient *sftp.Client, localCallBuddyPath, remot
 	} else {
 		// We don't know if the existing binary is old, so we'll remove it and
 		// replace it with a new one
-		// FIXME DG: What if ~/.call-buddy exists, but ~/.call-buddy/call-buddy does not?
 		err = remoteSftpClient.Remove(remoteCallBuddyPath)
 		if err != nil {
-			log.Printf("Failed to remove existing remote call-buddy path '%s': %s\n", remoteCallBuddyPath, err)
-			return err
+			// Catch the case where ~/.call-buddy exists, but ~/.call-buddy/call-buddy does not
+			if !os.IsNotExist(err) {
+				log.Printf("Failed to remove existing remote call-buddy path '%s': %s\n", remoteCallBuddyPath, err)
+				return err
+			}
 		}
 	}
 	remoteExe, err := remoteSftpClient.Create(remoteCallBuddyPath)
@@ -187,13 +201,68 @@ func bootstrapCallBuddy(remoteSftpClient *sftp.Client, localCallBuddyPath, remot
 	return nil
 }
 
+// This is a best effort syncing function, so if we can't do things we're not
+// going to die :)
+func remoteSync(remoteSftpClient *sftp.Client, remoteState *remoteFileState) error {
+	files, err := remoteSftpClient.ReadDir(remoteState.remotePath)
+	if err != nil {
+		return err
+	}
+
+	var seenFilenames = make([]string, 1)
+	for _, file := range files {
+		filename := file.Name()
+
+		// Clever hack to ensure that if a file changes to a directory, we
+		// don't try and copy a directory rather than a file
+		if file.IsDir() {
+			filename += "/"
+		}
+		seenFilenames = append(seenFilenames, filename)
+
+		if filename == "call-buddy" {
+			continue
+		}
+
+		lastModTime, inMap := remoteState.modTimes[file.Name()]
+		modTime := file.ModTime()
+		if inMap && !modTime.After(lastModTime) {
+			continue
+		}
+
+		remoteFilepath := remoteState.remotePath + "/" + filename
+		remoteFd, err := remoteSftpClient.Open(remoteFilepath)
+		if err != nil {
+			log.Printf("Failed to remotely open %s for syncing\n", remoteFilepath)
+			continue
+		}
+
+		localFilepath := remoteState.localPath + "/" + filename
+		// FIXME DG: This is _really_ dangerous... We'll truncate existing files and if there are issues we'll end up destroying things!
+		localFd, err := os.Create(localFilepath)
+		if err != nil {
+			log.Printf("Failed to locally open %s for syncing\n", localFilepath)
+			continue
+		}
+		_, err = remoteFd.WriteTo(localFd)
+		if err != nil {
+			log.Fatalf("Failed to locally write %s for syncing\n", localFilepath)
+			continue
+		}
+
+		remoteState.modTimes[filename] = modTime
+		log.Printf("Sucessfully synced remote %s to local %s after it was modifed at %s\n", remoteFilepath, localFilepath, modTime)
+	}
+	return nil
+}
+
 func cleanupRemoteSyncing(remoteClient *remoteSyncClient) {
 	// TODO: Cleanup syncing goroutine
 	remoteClient.sshSession.Close()
 	remoteClient.sftpClient.Close()
 }
 
-func spawnRemoteSyncing(client *ssh.Client, remoteCallBuddyPath, arch *string) (*remoteSyncClient, error) {
+func spawnRemoteSyncing(client *ssh.Client, remoteCallBuddyDir string) (*remoteSyncClient, error) {
 	// FIXME: Log things out
 	syncingSession, err := client.NewSession()
 	if err != nil {
@@ -217,8 +286,47 @@ func spawnRemoteSyncing(client *ssh.Client, remoteCallBuddyPath, arch *string) (
 		syncingSession.Close()
 		return nil, err
 	}
-
 	remoteClient := remoteSyncClient{syncingSession, syncingClient}
+
+	cwd, _ := os.Getwd()
+	remoteState := remoteFileState{
+		localPath:  cwd,
+		remotePath: remoteCallBuddyDir,
+		modTimes:   make(map[string]time.Time),
+	}
+
+	syncPeriod := time.Duration(3)
+	ticker := time.NewTicker(syncPeriod * time.Second)
+	go func() {
+		for {
+			waitGroup.Add(1)
+			select {
+			case <-ticker.C:
+				err := remoteSync(syncingClient, &remoteState)
+
+				// We get a EOF when sftp closes
+				// FIXME DG: Catch an EOF rather than a general error so we can
+				//           be more tolerent
+				// FIXME DG: We're getting an EOF since we are closing the ssh
+				//		     session (which includes the sftp subsystem)
+				//           outside of this goroutine, so ideally we have some
+				//           syncronization primitive here to check whether the
+				//           ssh session has closed and exit. Or maybe there's
+				//           a way to externally shutdown a goroutine?
+				if err != nil {
+					ticker.Stop()
+					log.Printf("Failed to sync files: %s\n", err)
+				}
+				break
+			case <-quit:
+				log.Println("Quitting from the goroutine!")
+				ticker.Stop()
+				waitGroup.Done()
+				return
+			}
+			waitGroup.Done()
+		}
+	}()
 	return &remoteClient, nil
 }
 
@@ -240,8 +348,9 @@ func remoteRun(target, arch *string, args []string) {
 	}
 
 	remoteCallBuddyPath := remoteHomeDir + "/.call-buddy/call-buddy"
+	remoteCallBuddyDir := filepath.Dir(remoteCallBuddyPath)
 	log.Printf("Spawning off remote syncing client on %s\n", hostname)
-	syncingClient, err := spawnRemoteSyncing(client, &remoteCallBuddyPath, &detectedArch)
+	syncingClient, err := spawnRemoteSyncing(client, remoteCallBuddyDir)
 	if err != nil {
 		// TODO: Inspect the error and don't always fail
 		log.Fatal("Failed to spawn off remote syncing")
@@ -249,7 +358,7 @@ func remoteRun(target, arch *string, args []string) {
 
 	cwd, _ := os.Getwd()
 	localCallBuddyPath := filepath.Clean(cwd + "/../telephono-ui/build/" + *arch + "/call-buddy")
-	fmt.Printf("Syncing call-buddy from %s to remote client at %s@%s:%s\n", localCallBuddyPath, username, hostname, remoteCallBuddyPath)
+	log.Printf("Syncing call-buddy from %s to remote client at %s@%s:%s\n", localCallBuddyPath, username, hostname, remoteCallBuddyPath)
 	err = bootstrapCallBuddy(syncingClient.sftpClient, localCallBuddyPath, remoteCallBuddyPath)
 	bootstrapped := err == nil
 
@@ -264,6 +373,9 @@ func remoteRun(target, arch *string, args []string) {
 	if bootstrapped {
 		cleanupBootstrapCallBuddy(syncingClient.sftpClient, remoteCallBuddyPath)
 	}
+	log.Println("Got here!")
+	quit <- struct{}{}
+	waitGroup.Wait()
 	session.Close()
 	cleanupRemoteSyncing(syncingClient)
 }
